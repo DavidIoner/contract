4fec554cac7e2b4cd097f16b25cdbc713636b81a
David Ioner <davidhioner@gmail.com>
2022-02-02 00:22:17 -0300

    templates

--- test.py
+++ test.py
@@ -5,10 +5,10 @@ import os
 # get the path of the current file
 path = os.path.dirname(os.path.realpath(__file__))
 
-TEMPLATE = path + "/stylesheet/page_01.html"
+TEMPLATE = path + "/stylesheet/page_02.html"
 
 font_config = FontConfiguration()
 html = HTML(TEMPLATE)
 css = CSS(filename=path + "/stylesheet/styles.css")
 
-html.write_pdf(path + '/tmp/CRE.pdf', font_config=font_config, stylesheets=[css])
+html.write_pdf(path + '/tmp/page_02.pdf', font_config=font_config, stylesheets=[css])
