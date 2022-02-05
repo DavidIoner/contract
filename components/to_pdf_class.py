393da13a7c4c6c253e3143d35183d2df81cbb07d
David Ioner <davidhioner@gmail.com>
2022-02-05 17:46:56 -0300

    working v1

--- components/to_pdf_class.py
+++ components/to_pdf_class.py
@@ -15,9 +15,7 @@ def get_rate(id="MXN-BRL"):
 
 class Report:
     def __init__(self, vars_dict={}):
-    # get the path of the current file
         self.vars_dict = vars_dict
-        #self.template_name = template_name
         self.ROOT = os.path.dirname(os.path.abspath(__file__))
         self.TEMPLATE_SRC = os.path.join(self.ROOT, 'templates')
         self.DEST_DIR = os.path.join(self.ROOT, 'output')
@@ -29,26 +27,24 @@ class Report:
         env = Environment(loader=FileSystemLoader(self.TEMPLATE_SRC))
         template = env.get_template(template_file)
         css = os.path.join(self.TEMPLATE_SRC, 'styles.css')
+        
         # variables
-        print('carregou template')
         BRL = get_rate('BRL-USD')
         MXN = get_rate('MXN-USD')
         COP = get_rate('COP-USD')
         security_USD = float(self.vars_dict['security_MXN']) * MXN
         wage_USD = float(self.vars_dict['wage_MXN']) * MXN
         christmas_USD = float(self.vars_dict['christmas_MXN']) * MXN
-        apartment_fee_USD = float(self.vars_dict['apartment_fee']) * MXN
+        apartment_price_USD = float(self.vars_dict['apartment_price_MXN']) * MXN
         self.vars_dict.update({'date': datetime.now().strftime('%d/%m/%Y')})
         self.vars_dict.update({'MXN': f'{MXN:.2f}', 'BRL': f'{BRL:.2f}', 'COP': f'{COP:.2f}'})
-        self.vars_dict.update({'security_USD': f'{security_USD:.2f}', 'wage_USD': f'{wage_USD:.2f}', 'christmas_USD': f'{christmas_USD:.2f}', 'apartment_fee_USD': f'{apartment_fee_USD:.2f}'})
+        self.vars_dict.update({'security_USD': f'{security_USD:.2f}', 'wage_USD': f'{wage_USD:.2f}', 'christmas_USD': f'{christmas_USD:.2f}', 'apartment_price_USD': f'{apartment_price_USD:.2f}'})
 
-        print("fase 1")
         # rendering to html string
         self.vars_dict['template_src'] = 'file://' + self.TEMPLATE_SRC
         rendered_string = template.render(self.vars_dict)
         html = HTML(string=rendered_string)
         report = os.path.join(self.DEST_DIR, output_name)
-        print("fase 2")
         html.write_pdf(report, stylesheets=[css])
         print(f'file is generated successfully and under {self.DEST_DIR}')
  
--- contract_generator.py
+++ contract_generator.py
@@ -14,13 +14,15 @@ class App(MDApp):
 
 
     def submit(self):
-        var_dict = {'licensee': self.root.ids.licensee.text,'start_date': self.root.ids.start_date.text, 'end_date': self.root.ids.end_date.text, 'location': self.root.ids.location.text, 'apartment_city': self.root.ids.apartment_city.text, 'apartment_price': self.root.ids.apartment_price.text, 'licensor': self.root.ids.licensor.text, 'onboarding': self.root.ids.onboarding.text, 'security_MXN': self.root.ids.security_MXN.text, 'wage_MXN': self.root.ids.wage_MXN.text, 'christmas_MXN': self.root.ids.christmas_MXN.text, 'apartment_fee': self.root.ids.apartment_fee.text,}
+        var_dict = {'licensee': self.root.ids.licensee.text,'start_date': self.root.ids.start_date.text, 'end_date': self.root.ids.end_date.text,'company': self.root.ids.company.text,'located_at': self.root.ids.located_at.text,'phone': self.root.ids.phone.text, 'location': self.root.ids.location.text, 'apartment_price_MXN': self.root.ids.apartment_price_MXN.text, 'licensor': self.root.ids.licensor.text, 'onboarding': self.root.ids.onboarding.text, 'security_MXN': self.root.ids.security_MXN.text, 'wage_MXN': self.root.ids.wage_MXN.text, 'christmas_MXN': self.root.ids.christmas_MXN.text,}
         try:
             name = var_dict['licensee']
             contract = pdf.Report(vars_dict=var_dict)
             contract.start('CRE_contract.html', f'CRE_{name}.pdf')
+            self.root.ids.label.text = f'Contract for {name} is generated successfully'
         except:
             print('error')
+            self.root.ids.label.text = 'Error'
 
 
 
