b90232405b28c47a2dc83796627cd1ba1e3d31ac
David Ioner <davidhioner@gmail.com>
2022-02-08 04:23:27 -0300

    v01

--- components/to_pdf_class.py
+++ components/to_pdf_class.py
@@ -3,6 +3,7 @@ import os
 from jinja2 import Environment, FileSystemLoader
 import requests
 from datetime import datetime
+from components.send_pdf import send_email
 
 # come back one directory
 def get_rate(id="MXN-BRL"):
@@ -20,7 +21,6 @@ class Report:
         self.TEMPLATE_SRC = os.path.join(self.ROOT, 'templates')
         self.DEST_DIR = os.path.join(self.ROOT, 'output')
        
-        print(self.vars_dict)
 
     def start(self, template_file, output_name=False):
         print('start generate report...')
@@ -33,13 +33,27 @@ class Report:
         BRL = get_rate('BRL-USD')
         MXN = get_rate('MXN-USD')
         COP = get_rate('COP-USD')
-        security_USD = float(self.vars_dict['security_MXN']) * MXN
+        if 'USD' in self.vars_dict['security']:
+            security_temp = self.vars_dict['security'].replace('USD', '')
+            security_USD = float(security_temp)
+            security_MXN = security_USD / MXN
+        else:
+            security_temp = self.vars_dict['security'].replace('MXN', '')
+            security_MXN = float(security_temp)
+            security_USD = security_MXN * MXN
+
         wage_USD = float(self.vars_dict['wage_MXN']) * MXN
         christmas_USD = float(self.vars_dict['christmas_MXN']) * MXN
-        apartment_price_USD = float(self.vars_dict['apartment_price_MXN']) * MXN
-        self.vars_dict.update({'date': datetime.now().strftime('%d/%m/%Y')})
+        apartment_price_USD = float(self.vars_dict['apartment_price_USD'])
+        federal_holiday_USD = float(self.vars_dict['federal_holiday_MXN']) * MXN
+
+        end_date = self.vars_dict['start_date']
+        end_year = int(end_date[-4:]) + 1
+        end_date = end_date[:-4] + str(end_year)
+        self.vars_dict.update({'date': datetime.now().strftime('%d/%m/%Y'), 'end_date': str(end_date)})
         self.vars_dict.update({'MXN': f'{MXN:.2f}', 'BRL': f'{BRL:.2f}', 'COP': f'{COP:.2f}'})
-        self.vars_dict.update({'security_USD': f'{security_USD:.2f}', 'wage_USD': f'{wage_USD:.2f}', 'christmas_USD': f'{christmas_USD:.2f}', 'apartment_price_USD': f'{apartment_price_USD:.2f}'})
+        self.vars_dict.update({'security_USD': f'{security_USD:.2f}', 'security_MXN': f'{security_MXN:.2f}', 'wage_USD': f'{wage_USD:.2f}', 'christmas_USD': f'{christmas_USD:.2f}', 'apartment_price_USD': f'{apartment_price_USD:.2f}', 'federal_holiday_USD': f'{federal_holiday_USD:.2f}'})
+        print(self.vars_dict)
         print('rendering')
         # rendering to html string
         self.vars_dict['template_src'] = 'file://' + self.TEMPLATE_SRC
@@ -49,6 +63,8 @@ class Report:
         print('generating pdf')
         html.write_pdf(report, stylesheets=[css])
         print(f'file is generated successfully and under {self.DEST_DIR}')
+        print('sending email')
+        send_email(report)
  
 
 
--- contract_generator.py
+++ contract_generator.py
@@ -2,19 +2,75 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 import components.to_pdf_class as pdf
 
+from kivy.metrics import dp
+from kivymd.uix.menu import MDDropdownMenu
 
 
 class App(MDApp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.kv = Builder.load_file("components/ui.kv")
+        menu_items = [
+            {
+                "viewclass": "OneLineIconListItem",
+                "text": f"local 1",
+                "height": dp(56),
+                "on_release": lambda x=f"#1 2DA CERRADA LAGO BOLSENA": self.set_item(x),
+            },
+            {
+                "viewclass": "OneLineIconListItem",
+                "text": f"local 2 (not available)",
+                "height": dp(56),
+                "on_release": lambda x=f"#2": self.set_item(x),
+            }, 
+            {
+                "viewclass": "OneLineIconListItem",
+                "text": f"local 3 (not available)",
+                "height": dp(56),
+                "on_release": lambda x=f"#3": self.set_item(x),
+            } 
+        ]
+        self.menu = MDDropdownMenu(
+            caller=self.kv.ids.drop_local,
+            items=menu_items,
+            position="center",
+            width_mult=4,
+        )
+        self.menu.bind()
 
-    def build(self):
-        kv = Builder.load_file("components/ui.kv")
-        return kv
+        # to avoid bugs
+        self.security_check = False
+
+    def set_item(self, text_item):
+        self.kv.ids.drop_local.set_item(text_item)
+        self.menu.dismiss()
+        print(text_item)
+        if "#1" in text_item:
+            self.specific_location = "2DA CERRADA LAGO BOLSENA #54, LAGO NORTE DF, MIGUEL HIDALGO, DF. MX"
+            self.location = "Mexico City, Mexico"
+            self.city = "Mexico City"
+        else:
+            self.specific_location = text_item
+    
+    
+    def check(self, checkbox, active):
+        print(active)
+        if active:
+            self.security_check = True
+            self.root.ids.security.hint_text = 'Security deposit (USD)'
+        if not active:
+            self.security_check = False     
+            self.root.ids.security.hint_text = 'Security deposit (MXN)'   
+
+        
 
 
     def submit(self):
-        var_dict = {'licensee': self.root.ids.licensee.text,'start_date': self.root.ids.start_date.text, 'end_date': self.root.ids.end_date.text,'company': self.root.ids.company.text,'located_at': self.root.ids.located_at.text,'phone': self.root.ids.phone.text, 'location': self.root.ids.location.text, 'apartment_price_MXN': self.root.ids.apartment_price_MXN.text, 'licensor': self.root.ids.licensor.text, 'onboarding': self.root.ids.onboarding.text, 'security_MXN': self.root.ids.security_MXN.text, 'wage_MXN': self.root.ids.wage_MXN.text, 'christmas_MXN': self.root.ids.christmas_MXN.text,}
+        if self.security_check:
+            self.security = self.root.ids.security.text + 'USD'
+        if not self.security_check:
+            self.security = self.root.ids.security.text + 'MXN'
+        var_dict = {'licensee': self.root.ids.licensee.text,'start_date': self.root.ids.start_date.text, 'company': self.root.ids.company.text,'located_at': self.root.ids.located_at.text,'phone': self.root.ids.phone.text, 'location': self.location, 'specific_location': self.specific_location,'city': self.city, 'apartment_price_USD': self.root.ids.apartment_price_USD.text, 'licensor': self.root.ids.licensor.text, 'onboarding': self.root.ids.onboarding.text, 'security': self.security, 'wage_MXN': self.root.ids.wage_MXN.text, 'christmas_MXN': self.root.ids.christmas_MXN.text, 'federal_holiday_MXN': self.root.ids.federal_holiday_MXN.text}
         try:
             name = var_dict['licensee']
             contract = pdf.Report(vars_dict=var_dict)
@@ -25,5 +81,7 @@ class App(MDApp):
             self.root.ids.label.text = 'Error'
 
 
+    def build(self):
+        return self.kv
 
 App().run()
